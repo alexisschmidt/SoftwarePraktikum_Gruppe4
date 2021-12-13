7becae8ac1e821c059366432a091a1c04b08a393
Deniz Gazitepe <93332279+DenizGazitepe@users.noreply.github.com>
2021-12-13 17:01:03 +0100

    fixed issues regarding datetime import

--- backend/server/bo/Spo.py
+++ backend/server/bo/Spo.py
@@ -1,11 +1,12 @@
 from server.bo.NamedBo import NamedBo
 """Die "date" Klasse aus dem Modul "datetime" wird importiert um das datumsformat für Variablen zu verwenden"""
-from datetime import date
+import datetime
+
 
 class Spo (NamedBo):
-    
+
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.start_date = datetime.date(1, 1, 1)    # Anfangsdatum der SPO Gültigkeit
         self.end_date = datetime.date(1, 1, 1)  # Enddatum der SPO Gültigkeit
 
@@ -16,7 +17,6 @@ class Spo (NamedBo):
     def set_start_date(self, year, month, day):
         """Bestimmen des Anfangsdatums der SPO Gültigkeit """
         self.start_date = datetime.date(year, month, day)
-
 
     def get_end_date(self):
         """Auslesen des Enddatums der SPO Gültigkeit """
