5e77d3e0e4a14d97bc8e21315cab4589c73d17c5
Deniz Gazitepe <93332279+DenizGazitepe@users.noreply.github.com>
2021-12-13 17:39:39 +0100

    added class specific attributes and corresponding get and set methods

--- backend/server/bo/SpoElement.py
+++ backend/server/bo/SpoElement.py
@@ -1,5 +1,31 @@
 from server.bo.NamedBo import NamedBo
 
+
 class SpoElement (NamedBo):
+    edvnr: int
+    ects: int
+    workload: str
+
     def __init__(self):
-        super.__init__()
+        super().__init__()
+        self.edvnr = 0
+        self.ects = 0
+        self.workload = ""
+
+    def get_edvnr(self):
+        return self.edvnr
+
+    def set_edvnr(self, edvnr):
+        self.edvnr = edvnr
+
+    def get_ects(self):
+        return self.ects
+
+    def set_ects(self, ects):
+        self.ects = ects
+
+    def get_workload(self):
+        return self.workload
+
+    def set_workload(self, workload):
+        self.workload = workload
