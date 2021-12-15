bd19a753390583a1078cc49bec98e76ebf2c1165
SebastianHennich <sh293@hdm-stuttgart.de>
2021-12-15 10:31:30 +0100

    Dumpverbesserung und Mapper

---
+++ backend/server/db/ModulePartMapper.py
@@ -0,0 +1,166 @@
+"""HIER BO IMPORTIEREN"""
+from backend.server.db.Mapper import Mapper
+
+
+class ModuleMapper(Mapper):
+
+    def __init__(self):
+        super().__init__()
+
+    def find_all(self):
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT * from modulepart")
+        tuples = cursor.fetchall()
+
+        for (
+                id, creationdate, name, title, language, literature, semester_id, sources, connection, description, sws,
+                ects,
+                edvnr, workload) \
+                in tuples:
+            modulepart = ModulePart()
+            modulepart.set_id(id)
+            modulepart.set_name(name)
+            modulepart.set_title(title)
+            modulepart.set_language(language)
+            modulepart.set_literature(literature)
+            modulepart.set_semester_id(semester_id)
+            modulepart.set_sources(sources)
+            modulepart.set_connection(connection)
+            modulepart.set_description(description)
+            modulepart.set_sws(sws)
+            modulepart.set_ects(ects)
+            modulepart.set_edvnr(edvnr)
+            modulepart.set_workload(workload)
+
+            result.append(modulepart)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_name(self, name):
+
+        result = []
+        cursor = self._cnx.cursor()
+        command = "SELECT * FROM modulepart WHERE name LIKE '{}' ORDER BY name".format(name)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        for (
+                id, creationdate, name, title, language, literature, semester_id, sources, connection, description, sws,
+                ects,
+                edvnr, workload) in tuples:
+            modulepart = ModulePart()
+            modulepart.set_id(id)
+            modulepart.set_name(name)
+            modulepart.set_title(title)
+            modulepart.set_language(language)
+            modulepart.set_literature(literature)
+            modulepart.set_semester_id(semester_id)
+            modulepart.set_sources(sources)
+            modulepart.set_connection(connection)
+            modulepart.set_description(description)
+            modulepart.set_sws(sws)
+            modulepart.set_ects(ects)
+            modulepart.set_edvnr(edvnr)
+            modulepart.set_workload(workload)
+
+            result.append(modulepart)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_key(self, key):
+
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT * modulepart WHERE id={}".format(key)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (
+                id, creationdate, name, title, language, literature, semester_id, sources, connection, description, sws,
+                ects,
+                edvnr, workload) = tuples[0]
+            modulepart = ModulePart()
+            modulepart.set_id(id)
+            modulepart.set_name(name)
+            modulepart.set_title(title)
+            modulepart.set_language(language)
+            modulepart.set_literature(literature)
+            modulepart.set_semester_id(semester_id)
+            modulepart.set_sources(sources)
+            modulepart.set_connection(connection)
+            modulepart.set_description(description)
+            modulepart.set_sws(sws)
+            modulepart.set_ects(ects)
+            modulepart.set_edvnr(edvnr)
+            modulepart.set_workload(workload)
+            result = modulepart
+        except IndexError:
+
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def insert(self, modulepart):
+
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM modulepart ")
+        tuples = cursor.fetchall()
+
+        for (maxid) in tuples:
+            if maxid[0] is not None:
+
+                module.set_id(maxid[0] + 1)
+            else:
+
+                module.set_id(1)
+
+        command = "INSERT INTO module (id, creationdate, name, title, language, literature, semester_id, sources, connection, description, sws, ects, edvnr, workload) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) "
+        data = (modulepart.get_id(), modulepart.get_name(), modulepart.get_title(), modulepart.get_language(),
+                modulepart.get_literature(), modulepart.get_semester_id(), modulepart.get_sources(),
+                modulepart.get_connection(), modulepart.get_description(), modulepart.get_sws(), modulepart.get_ects(),
+                modulepart.get_edvnr(), modulepart.get_workload())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return modulepart
+
+    def update(self, modulepart):
+
+        cursor = self._cnx.cursor()
+
+        command = "UPDATE modulepart " + "SET name=%s, SET title=%s, SET language=%s, SET literature=%s, " \
+                                         "SET semester_id=%s, SET sources=%s, SET connection=%s, SET description=%s, " \
+                                         "SET sws=%s, SET ects=%s,SET edvnr=%s,SET workload=%s WHERE id=%s "
+        data = (
+            modulepart.get_name(), modulepart.get_title(), modulepart.get_language(),
+            modulepart.get_literature(), modulepart.get_semester_id(), modulepart.get_sources(),
+            modulepart.get_connection(), modulepart.get_description(), modulepart.get_sws(), modulepart.get_ects(),
+            modulepart.get_edvnr(), modulepart.get_workload(), modulepart.get_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+    def delete(self, modulepart):
+
+        cursor = self._cnx.cursor()
+
+        command = "DELETE FROM modulepart WHERE id={}".format(modulepart.get_id())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
---
+++ backend/server/db/PersonMapper.py
@@ -0,0 +1,129 @@
+"""HIER BO IMPORTIEREN"""
+from backend.server.db.Mapper import Mapper
+
+
+class PersonMapper(Mapper):
+
+    def __init__(self):
+        super().__init__()
+
+    def find_all(self):
+
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT * from person")
+        tuples = cursor.fetchall()
+
+        for (id, creationdate, name, title, firstname, lastname, email) in tuples:
+            person = Person()
+            person.set_id(id)
+            person.set_name(name)
+            person.set_title(title)
+            person.set_firstname(firstname)
+            person.set_lastname(lastname)
+            person.set_email(email)
+
+            result.append(person)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_name(self, name):
+
+        result = []
+        cursor = self._cnx.cursor()
+        command = "SELECT * FROM person WHERE name LIKE '{}' ORDER BY name".format(name)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        for (id, creationdate, name, title, firstname, lastname, email) in tuples:
+            person = Person()
+            person.set_id(id)
+            person.set_name(name)
+            person.set_title(title)
+            person.set_firstname(firstname)
+            person.set_lastname(lastname)
+            person.set_email(email)
+
+            result.append(person)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_key(self, key):
+
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT * person WHERE id={}".format(key)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, creationdate, name, title, firstname, lastname, email) = tuples[0]
+            person = Person()
+            person.set_id(id)
+            person.set_name(name)
+            person.set_title(title)
+            person.set_firstname(firstname)
+            person.set_lastname(lastname)
+            person.set_email(email)
+            result = person
+        except IndexError:
+
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def insert(self, person):
+
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM person ")
+        tuples = cursor.fetchall()
+
+        for (maxid) in tuples:
+            if maxid[0] is not None:
+
+                person.set_id(maxid[0] + 1)
+            else:
+
+                person.set_id(1)
+
+        command = "INSERT INTO person (id, creationdate, name, title, firstname, lastname, email) VALUES (%s,%s,%s,%s,%s,%s,%s) "
+        data = (person.get_id(), person.get_creationdate(), person.get_name(), person.get_title(), person.get_firstname(), person.get_lastname(), person.get_email())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return person
+
+    def update(self, person):
+
+        cursor = self._cnx.cursor()
+
+        command = "UPDATE person " + "SET name=%s, SET title=%s, SET firstname=%s, SET lastname=%s, SET email=%s WHERE id=%s "
+        data = (person.get_name(), person.get_title(), person.get_firstname(), person_get.lastname(), person_getemail(), person.get_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+    def delete(self, person):
+
+        cursor = self._cnx.cursor()
+
+        command = "DELETE FROM person WHERE id={}".format(person.get_id())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
+
+
