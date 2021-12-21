aa3b838df9189324836d3701a6678cd2af23107a
Deniz Gazitepe <93332279+DenizGazitepe@users.noreply.github.com>
2021-12-21 21:35:16 +0100

    added imports of BOs for later use

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,20 @@ from flask import Flask
 from flask_restx import Api, Resource, fields
 from flask_cors import CORS
 
+# Wir greifen natürlich auf unsere Applikationslogik inkl. BusinessObject-Klassen zurück
+from server.bo.Businessobject import BusinessObject
+from server.bo.NamedBo import NamedBo
+from server.bo.Spo import Spo
+from server.bo.SpoElement import  SpoElement
+from server.bo.Module import Module
+from server.bo.Modulepart import Modulepart
+from server.bo.StudyCourse import StudyCourse
+from server.bo.Person import Person
+from server.bo.User import User
+
 from server.Administration import Administration
+
+# Außerdem nutzen wir einen selbstgeschriebenen Decorator der die Authentifikation übernimmt
 from server.SecurityDecorator import secured
 
 app = Flask(__name__)
@@ -26,6 +39,17 @@ bo = api.model('BusinessObject', {
 })
 
 """Alle anderen BusinessObjects"""
+namedbo = api.inherit('Namedbo', bo){
+    'name': fields.string(attribute='_name', description='Name eines NamedBOs')
+    'title': fields.string(attribute='_name', description='Titel eines NamedBOs')
+}
+
+spo = api.inherit('Spo', namedbo){
+    'start_date': fields.date(attribute='_start_date', description='Anfangsdatum der SPO-gültigkeit'),
+    'end_date': fields.date(attribute='_end_date', description='Enddatum der SPO-gültigkeit')
+}
+
+
 
 
 """Alles @sposystem.route('')"""
