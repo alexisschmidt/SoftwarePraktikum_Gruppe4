e359f47feb2f61bdb69ed10df2bf081ab8b56ee0
Deniz Gazitepe <93332279+DenizGazitepe@users.noreply.github.com>
2021-12-21 22:47:29 +0100

    changed names of object variables to fit naming convention (_"")

--- backend/server/bo/Spo.py
+++ backend/server/bo/Spo.py
@@ -11,8 +11,8 @@ class Spo (NamedBo):
 
     def __init__(self):
         super().__init__()
-        self.start_date = datetime.date(1, 1, 1)    # Anfangsdatum der SPO Gültigkeit
-        self.end_date = datetime.date(1, 1, 1)  # Enddatum der SPO Gültigkeit
+        self._start_date = datetime.date(1, 1, 1)    # Anfangsdatum der SPO Gültigkeit
+        self._end_date = datetime.date(1, 1, 1)  # Enddatum der SPO Gültigkeit
 
     def get_start_date(self):
         """Auslesen des Anfangsdatums der SPO Gültigkeit """
