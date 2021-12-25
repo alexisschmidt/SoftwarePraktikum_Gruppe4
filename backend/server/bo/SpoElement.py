2c79dd87c3b651b31c0cd69c07c6cc8225da971d
Deniz Gazitepe <93332279+DenizGazitepe@users.noreply.github.com>
2021-12-25 23:20:58 +0100

    fixed syntax and PEP8 errors, added comments,

--- backend/server/bo/SpoElement.py
+++ backend/server/bo/SpoElement.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from server.bo.NamedBo import NamedBo
 
 
@@ -9,26 +9,30 @@ class SpoElement (ABC, NamedBo):
 
     def __init__(self):
         super().__init__()
-        self.edvnr = 0
-        self.ects = 0
-        self.workload = ""
+        self.edvnr = 0      # EDV Nummer des Spo Elements
+        self.ects = 0       # ECTS des Spo Elements
+        self.workload = ""  # Arbeitsaufwand in Stunden des Spo Elements
 
     def get_edvnr(self):
+        """Auslesen der EDV Nummer"""
         return self.edvnr
 
     def set_edvnr(self, edvnr):
+        """Setzen der EDV Nummer"""
         self.edvnr = edvnr
 
     def get_ects(self):
+        """Auslesen der ECTS"""
         return self.ects
 
     def set_ects(self, ects):
+        """Setzen der ECTS"""
         self.ects = ects
 
     def get_workload(self):
+        """Auslesen des Arbeitsaufwands"""
         return self.workload
 
     def set_workload(self, workload):
+        """Setzen des Arbeitsaufwands"""
         self.workload = workload
-
-
