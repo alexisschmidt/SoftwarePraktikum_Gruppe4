78af8df35807309594cd32ea961e779e735064c0
Deniz Gazitepe <93332279+DenizGazitepe@users.noreply.github.com>
2021-12-25 23:18:27 +0100

    fixed syntax and PEP8 errors, fixed from_dict(), added comments,

--- backend/server/bo/Modulepart.py
+++ backend/server/bo/Modulepart.py
@@ -1,10 +1,17 @@
 from server.bo.NamedBo import NamedBo
-from server.bo.SpoElement import SpoElement
+
 
 class Modulepart (NamedBo):
-    def __init__(self):
-        super.__init__()
+    __SWS: str
+    __language: str
+    __description: str
+    __connection: str
+    __literature: str
+    __sources: str
+    __semester: str
 
+    def __init__(self):
+        super().__init__()
         self.__SWS = ""
         self.__language = ""
         self.__description = ""
@@ -15,52 +22,64 @@ class Modulepart (NamedBo):
 
         # Auslesen
 
-    def get_SWS(self):
+    def get_sws(self):
+        """Auslesen der Semesterwochenstunden"""
         return self.__SWS
 
-    def set_SWS(self, value):
-        self.__SWS = value
+    def set_sws(self, sws):
+        """Setzen der Semesterwochenstunden"""
+        self.__SWS = sws
 
     def get_language(self):
+        """Auslesen der Modulteilsprache"""
         return self.__language
 
-    def set_language(self, value):
-        self.__language = value
+    def set_language(self, language):
+        """Setzen der Modulteilsprache"""
+        self.__language = language
 
     def get_description(self):
+        """Auslesen der Modulteilbeschreibung"""
         return self.__description
 
-    def set_description(self, value):
-        self.__description = value
+    def set_description(self, description):
+        """Setzen der Modulteilbeschreibung"""
+        self.__description = description
 
     def get_connection(self):
+        """Auslesen der Modulverknüpfung(en)"""
         return self.__connection
 
     def set_connection(self, value):
+        """Setzen der Modulverknüpfung(en)"""
         self.__connection = value
 
     def get_literature(self):
+        """Auslesen der Lektüre"""
         return self.__literature
 
-    def set_literature(self, value):
-        self.__literature = value
+    def set_literature(self, literature):
+        """Setzen der Lektüre"""
+        self.__literature = literature
 
     def get_sources(self):
+        """Auslesen der Quelle(n)"""
         return self.__sources
 
-    def set_sources(self, value):
-        self.__sources = value
-
+    def set_sources(self, sources):
+        """Setzen der Quelle(n)"""
+        self.__sources = sources
 
     def get_semester(self):
+        """Auslesen des Semesters"""
         return self.__semester
 
-    def set_semester(self, value):
-        self.__semester = value
+    def set_semester(self, semester):
+        """Setzen des Semesters"""
+        self.__semester = semester
 
     def __str__(self):
         return "Modulepart: {}, {}, {}, {}, {}, {}, {}, {}".format(
-
             self.get_id(),
             self.__SWS,
             self.__language,
@@ -74,9 +93,9 @@ class Modulepart (NamedBo):
     def from_dict(dictionary=dict()):
         """Umwandeln eines Python dict() in ein Modulepart()."""
         obj = Modulepart()
-        obj.set_id(dictionary["id"])
-        obj.set_SWS(dictionary["SWS"])
-        obj.set_language(dictionary["language"])
+        obj.set_id(dictionary["id"])                    # Teil von BusinessObject!
+        obj.set_sws(dictionary["SWS"])                  # Teil von NamedBo!
+        obj.set_language(dictionary["language"])        # Teil von NamedBo!
         obj.set_description(dictionary["description"])
         obj.set_connection(dictionary["connection"])
         obj.set_literature(dictionary["literature"])
