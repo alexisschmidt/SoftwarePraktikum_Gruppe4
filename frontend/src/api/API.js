555d0d6f19fba5a1700d93661658ac4b3fdf9f51
alexisschmidt <80887137+alexisschmidt@users.noreply.github.com>
2022-01-07 15:02:45 +0100

    add URLs

--- frontend/src/api/API.js
+++ frontend/src/api/API.js
@@ -1,20 +1,36 @@
-//Import BOs
+import Modulebo from './Modulebo';
+import Modulepartbo from './Modulepartbo';
+import Personbo from './Personbo';
+import Semesterbo from './Semesterbo';
+import Spobo from './Spobo';
+import SpoElementbo from './SpoElementbo';
+import StudyCoursebo from './StudyCoursebo';
+import Userbo from './Userbo';
 
 export default class API{
 	//Singleton instance
 		static #api = null;
 	
 		//local Python backend
-		//serverBaseURL = '/sopra';
+		#serverBaseURL = '/sopra';
 	
 		//Local http-fake-backend?
 	
-	
-		//... related
-		//getCustomersURL = () => `${this.#bankServerBaseURL}/customers`;
-	
-		//... related
-	
+		//Admin Ansicht - Neue Spo erstellen
+		#getSpoURL = (id) => `${this.#serverBaseURL}/spo/${id}`; //Die ausgewählte SPO wird dem Admin angezeigt abruf durch den hash? id? oder durch einzelne module?
+		#addModulepartURL = () => `${this.#serverBaseURL}/spomdulepart`; //Für den Edit-Button (Modules)
+		#addModuleURL = () => `${this.#serverBaseURL}/module`; //Module hinzugügen
+		#getAllModulesURL = (id) => `${this.#serverBaseURL}/allmodules/${id}` //Alle Module einer id
+		#addSpoURL = () => `${this.#serverBaseURL}/spo` //Spo erstellen
+
+		//Admin Ansicht - Liste aller SPOs
+		#getAllSpoRelatedURL = (id) => `${this.#serverBaseURL}/spo/${id}`; //Alle Spos die zu einem Sudiengang gehören gelistet nach WS20, SS21...
+
+		//Admin Ansicht - Studiengänge
+		#getAllStudycoursesURL = () => `${this.#serverBaseURL}/studycourses`;
+
+		//Studenten Ansicht - Zugeordnete SPO/Ausgewählte SPO
+		
 		/** 
 	   * Get the Singelton instance 
 	   * 
