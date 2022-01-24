965fc4de0a22fb902f52f69ec9836b07bb62b1ca
Sarah Rowan <80517568+sr168@users.noreply.github.com>
2022-01-24 23:14:35 +0100

    Businessobjekt.js
    getter und setter methode von createdby und creatdate angepasst

--- frontend/src/components/pages/Businessobject.js
+++ frontend/src/components/pages/Businessobject.js
@@ -1,39 +1,56 @@
 /**
  * Basisklasse für alle BusinessObjects, für die standardmäßig eine ID angegeben ist
  */
- export default class BusinessObject {
-
-    /**
-     * The null constructor.
-     */
-    constructor() {
-      this.id = 0;
-    }
-  
-    /**
-     * Setzt ID des Businessobjekts.
-     * 
-     * @param {*} aId - neue ID für Businessobjekt.
-     */
-    setID(aId) {
-      this.id = aId;
-    }
-  
-    /**
-     * Gibt die ID des Businessobjekts zurück.
-     */
-    getID() {
-      return this.id;
-    }
-  
-    /**
-     * Gibt einen representativen String des Objekts zurück.
-     */
-    toString() {
-      let result = '';
-      for (var prop in this) {
-        result += prop + ': ' + this[prop] + ' ';
-      }
-      return result;
-    }
+export default class BusinessObject {
+  /**
+   * The null constructor.
+   */
+  constructor() {
+    this.id = 0;
+    this.creationdate = null;
+    this.createdby = null;
   }
+
+  /**
+   * Setzt ID des Businessobjekts.
+   *
+   * @param {*} aId - neue ID für Businessobjekt.
+   */
+  setID(aId) {
+    this.id = aId;
+  }
+
+  setCreationdate(aCreationdate) {
+    this.creationdate = aCreationdate;
+  }
+
+  setCreatedby(aCreatedby) {
+    this.createdby = aCreatedby;
+  }
+
+  /**
+   * Gibt die ID des Businessobjekts zurück.
+   */
+  getID() {
+    return this.id;
+  }
+
+  getCreationdate() {
+    return this.creationdate;
+  }
+
+  getCreatedby() {
+    return this.createdby;
+  }
+
+  /**
+   * Gibt einen representativen String des Objekts zurück.
+   */
+  toString() {
+    let result = "";
+    for (var prop in this) {
+      result += prop + ": " + this[prop] + " ";
+    }
+    return result;
+  }
+}
