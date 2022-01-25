6291e3f7b895142f53ed46d575a7f2a65f3c631f
Theresa Kottmann <87066328+Theresa17@users.noreply.github.com>
2022-01-25 20:17:11 +0100

    Component SpoAuswählenStudent

---
+++ frontend/src/components/pages/SpoAuswählenStudent.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+const options = ['SS21', 'WS20/21'];
+
+export default function ControllableStates() {
+  const [value, setValue] = React.useState(options[0]);
+  const [inputValue, setInputValue] = React.useState('');
+
+  return (
+    <div>
+      <div><h3> Wie lautet deine Spo? </h3></div>
+      <br />
+      
+      <Autocomplete
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        id="controllable-states-demo"
+        options={options}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Spo" />}
+      />
+       <Stack spacing={2} direction="row">
+          <Button variant="contained">Bestätigen</Button>
+       </Stack>
+    </div>
+  );
+}
