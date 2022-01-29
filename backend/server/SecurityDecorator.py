62593d289a9b202409e61b9a79393a17a1b62177
Deniz Gazitepe <93332279+DenizGazitepe@users.noreply.github.com>
2022-01-29 16:20:01 +0100

    fixed claims.get("firstname") to ("name")

--- backend/server/SecurityDecorator.py
+++ backend/server/SecurityDecorator.py
@@ -3,7 +3,7 @@ from google.auth.transport import requests
 import google.oauth2.id_token
 from server.bo.User import User
 
-from server.Administration import Administration
+from server.Administration import Administration as Admin
 """Decorator zur Google Firebase-basierten Authentifizierung von Benutzern"""
 
 
@@ -28,27 +28,26 @@ def secured(function):
                 claims = google.oauth2.id_token.verify_firebase_token(
                     id_token, firebase_request_adapter)
                 if claims is not None:
-                    adm = Administration()
                     google_user_id = claims.get("user_id")
                     email = claims.get("email")
-                    firstname = claims.get("firstname")
+                    firstname = claims.get("name")
                     
                     if not firstname:
                         firstname = "Failed to get name"
                     
-                    user = adm.get_user_by_google_user_id(google_user_id)
+                    user = Admin.get_user_by_google_user_id(google_user_id)
                     if user is not None:
                         """Fall: Der Benutzer ist unserem System bereits bekannt."""
                         user.set_firstname(firstname)
                         user.set_email(email)
-                        adm.save_user(user)
+                        Admin.save_user(user)
                     else:
                         """Fall: Der Benutzer war bislang noch nicht eingelogged. """
                         user = User()
                         user.set_firstname(firstname)
                         user.set_email(email)
                         user.set_google_user_id(google_user_id)
-                        user = adm.create_user(user, 1)                    
+                        user = Admin.create_user(user)
                     if request.method == 'POST' or request.method == 'PUT':
                         kwargs['user'] = user
                     objects = function(*args, **kwargs)
